cbcc47e4cdbad27febaac09c32616a2969b76229
giffy1 <giffy1991@gmail.com>
2016-06-01 15:17:57 -0400

    Added several writers, in addition to the original ACCEL and GYRO files. The ACCEL and GYRO files were renamed to ACCELEROMETER_WEARABLE and GYRO_WEARABLE respectively; new files include ACCELEROMETER_PHONE, GYRO_PHONE, ACCELEROMETER_METAWEAR, GYRO_METAWEAR, PHONE_TO_METAWEAR_RSSI and WEARABLE_TO_METAWEAR_RSSI; REPORT file removed. The file writers are initialized in the constructor, after the loadPreferences() method is called to get the user's preferred save location. IntentFilter actions SEND_ACCELEROMETER and SEND_GYROSCOPE consolidated into one single action BROADCAST_SESNOR_DATA, which also encapsulates all other sensor data; action SEND_LABEL removed. Receiver should be unregistered when stop service intent action is received, not when the service is destroyed. Also changed notification content title, ticker, content text and icon.

--- mobile/src/main/java/edu/umass/cs/prepare/storage/DataWriterService.java
+++ mobile/src/main/java/edu/umass/cs/prepare/storage/DataWriterService.java
@@ -7,25 +7,27 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.io.BufferedWriter;
+import java.io.File;
 
+import edu.umass.cs.shared.DataLayerUtil;
+import edu.umass.cs.shared.SharedConstants;
 import edu.umass.cs.prepare.R;
 import edu.umass.cs.prepare.constants.Constants;
 import edu.umass.cs.prepare.main.MainActivity;
 import edu.umass.cs.prepare.metawear.DataReceiverService;
-import edu.umass.cs.prepare.metawear.RemoteSensorManager;
 
 /**
- * The Data Writer Service is the main service on the handheld application. It is responsible for writing
- * the sensor data along with the labels to their respective files. It is a foreground service, allowing
- * the user to close the main UI while still collecting data.
+ * The Data Writer Service is responsible for writing all sensor data to their respective files.
  *
  * @author Sean Noran 7/7/15.
  * @see DataReceiverService
@@ -34,57 +36,87 @@ import edu.umass.cs.prepare.metawear.RemoteSensorManager;
  */
 public class DataWriterService extends Service {
 
+    @SuppressWarnings("unused")
+    /** used for debugging purposes */
     private static final String TAG = DataWriterService.class.getName();
 
-    /** Name of the file to which to write the accelerometer data */
-    private static final String ACCEL_TAG = "ACCEL";
+    private final BufferedWriter accelerometerWearableWriter;
+    private final BufferedWriter gyroscopeWearableWriter;
+    private final BufferedWriter accelerometerPhoneWriter;
+    private final BufferedWriter gyroscopePhoneWriter;
+    private final BufferedWriter accelerometerMetawearWriter;
+    private final BufferedWriter gyroscopeMetawearWriter;
+    private final BufferedWriter rssiMetawearToPhoneWriter;
+    private final BufferedWriter rssiMetawearToWearableWriter;
 
-    /** Name of the file to which to write the gyroscope data */
-    private static final String GYRO_TAG = "GYRO";
+    private File directory;
 
-    /** Name of the file to which to write the reported labels */
-    private static final String LABEL_TAG = "REPORT";
+    public DataWriterService(){
+        loadPreferences();
+        accelerometerWearableWriter = FileUtil.getFileWriter(DataWriterService.this, "ACCELEROMETER_WEARABLE", directory);
+        gyroscopeWearableWriter = FileUtil.getFileWriter(DataWriterService.this, "GYRO_WEARABLE", directory);
+        accelerometerPhoneWriter = FileUtil.getFileWriter(DataWriterService.this, "ACCELEROMETER_PHONE", directory);
+        gyroscopePhoneWriter = FileUtil.getFileWriter(DataWriterService.this, "GYRO_PHONE", directory);
+        accelerometerMetawearWriter = FileUtil.getFileWriter(DataWriterService.this, "ACCELEROMETER_METAWEAR", directory);
+        gyroscopeMetawearWriter = FileUtil.getFileWriter(DataWriterService.this, "GYRO_METAWEAR", directory);
+        rssiMetawearToWearableWriter = FileUtil.getFileWriter(DataWriterService.this, "WEARABLE_TO_METAWEAR_RSSI", directory);
+        rssiMetawearToPhoneWriter = FileUtil.getFileWriter(DataWriterService.this, "PHONE_TO_METAWEAR_RSSI", directory);
+    }
 
-    /** Buffered writer used to log the accelerometer data */
-    private final BufferedWriter accelWriter = null; // FileUtil.getFileWriter(ACCEL_TAG); //TODO: FIX THESE
-
-    /** Buffered writer used to log the gyroscope data */
-    private final BufferedWriter gyroWriter = null; //FileUtil.getFileWriter(GYRO_TAG);
-
-    /** Buffered writer used to log the reported labels */
-    private final BufferedWriter labelWriter = null; // FileUtil.getFileWriter(LABEL_TAG);
+    private void loadPreferences(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final String defaultDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), getString(R.string.app_name)).getAbsolutePath();
+        final String dir = preferences.getString(getString(R.string.pref_directory_key), defaultDirectory);
+        if (dir == null)
+            directory = new File(defaultDirectory);
+        else
+            directory = new File(dir);
+    }
 
     /** used to receive messages from other components of the handheld app through intents, i.e. receive labels **/
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction() != null) {
-                //TODO: fix and uncomment
-//                if (intent.getAction().equals(Constants.ACTION.SEND_ACCELEROMETER)) {
-//                    String line = intent.getStringExtra(Constants.VALUES.SENSOR_DATA);
-//                    synchronized (accelWriter) {
-//                        FileUtil.writeToFile(line, accelWriter);
-//                    }
-//                }else if (intent.getAction().equals(Constants.ACTION.SEND_GYROSCOPE)){
-//                    String line = intent.getStringExtra(Constants.VALUES.SENSOR_DATA);
-//                    synchronized (gyroWriter) {
-//                        FileUtil.writeToFile(line, gyroWriter);
-//                    }
-//                }else if (intent.getAction().equals(Constants.ACTION.SEND_LABEL)) {
-//                    String line = intent.getStringExtra(Constants.VALUES.LABEL);
-//                    synchronized (labelWriter) {
-//                        FileUtil.writeToFile(line, labelWriter);
-//                    }
-//                    try {
-//                        //to make sure the labels appear readily, flush the writer. We can do this
-//                        //with the label writer right away because labels are infrequent enough. We
-//                        //do not need to do this with the gyro/accel writers because the data is so
-//                        //frequent that the buffer fills and is flushed automatically
-//                        labelWriter.flush();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
+                if (intent.getAction().equals(Constants.ACTION.BROADCAST_SENSOR_DATA)) {
+                    String line = intent.getStringExtra(Constants.KEY.SENSOR_DATA);
+                    SharedConstants.SENSOR_TYPE sensorType = DataLayerUtil.deserialize(SharedConstants.SENSOR_TYPE.class).from(intent);
+                    if (sensorType == SharedConstants.SENSOR_TYPE.ACCELEROMETER_WEARABLE){
+                        synchronized (accelerometerWearableWriter) {
+                            FileUtil.writeToFile(line, accelerometerWearableWriter);
+                        }
+                    } else if (sensorType == SharedConstants.SENSOR_TYPE.GYROSCOPE_WEARABLE){
+                        synchronized (gyroscopeWearableWriter) {
+                            FileUtil.writeToFile(line, gyroscopeWearableWriter);
+                        }
+                    } else if (sensorType == SharedConstants.SENSOR_TYPE.ACCELEROMETER_PHONE){
+                        synchronized (accelerometerPhoneWriter) {
+                            FileUtil.writeToFile(line, accelerometerPhoneWriter);
+                        }
+                    } else if (sensorType == SharedConstants.SENSOR_TYPE.GYROSCOPE_PHONE){
+                        synchronized (gyroscopePhoneWriter) {
+                            FileUtil.writeToFile(line, gyroscopePhoneWriter);
+                        }
+                    }
+                    else if (sensorType == SharedConstants.SENSOR_TYPE.ACCELEROMETER_METAWEAR){
+                        synchronized (accelerometerMetawearWriter) {
+                            Log.d(TAG, line);
+                            FileUtil.writeToFile(line, accelerometerMetawearWriter);
+                        }
+                    } else if (sensorType == SharedConstants.SENSOR_TYPE.GYROSCOPE_METAWEAR){
+                        synchronized (gyroscopeMetawearWriter) {
+                            FileUtil.writeToFile(line, gyroscopeMetawearWriter);
+                        }
+                    } else if (sensorType == SharedConstants.SENSOR_TYPE.WEARABLE_TO_METAWEAR_RSSI){
+                        synchronized (rssiMetawearToWearableWriter) {
+                            FileUtil.writeToFile(line, rssiMetawearToWearableWriter);
+                        }
+                    } else if (sensorType == SharedConstants.SENSOR_TYPE.PHONE_TO_METAWEAR_RSSI){
+                        synchronized (rssiMetawearToPhoneWriter) {
+                            FileUtil.writeToFile(line, rssiMetawearToPhoneWriter);
+                        }
+                    }
+                }
             }
         }
     };
@@ -93,37 +125,14 @@ public class DataWriterService extends Service {
     public void onCreate(){
         //the intent filter specifies the messages we are interested in receiving
         IntentFilter filter = new IntentFilter();
-        //TODO: Get this back again
-//        filter.addAction(Constants.ACTION.SEND_ACCELEROMETER);
-//        filter.addAction(Constants.ACTION.SEND_GYROSCOPE);
-//        filter.addAction(Constants.ACTION.SEND_LABEL);
-
+        filter.addAction(Constants.ACTION.BROADCAST_SENSOR_DATA);
         registerReceiver(receiver, filter);
-    }
-
-    //TODO: According to the Android Documentation there is no guarantee that onDestroy() will ever
-    //be called... especially if the process is killed by the OS due to low memory. So we may want
-    //to flush and close the file writers somewhere else, otherwise we may lose recent accel/gyro
-    //data since it is not being flushed regularly (only when buffer is full)
-
-    @Override
-    public void onDestroy(){
-        Log.v(TAG, "onDestroy()");
-        unregisterReceiver(receiver);
-        if (accelWriter != null)
-            FileUtil.closeWriter(accelWriter);
-        if (gyroWriter != null)
-            FileUtil.closeWriter(gyroWriter);
-        if (labelWriter != null)
-            FileUtil.closeWriter(labelWriter);
-
-        RemoteSensorManager.getInstance(this).stopSensorService();
-        super.onDestroy();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
-        if (intent.getAction().equals(Constants.ACTION.START_FOREGROUND)) {
+        if (intent == null) return START_STICKY;
+        if (intent.getAction().equals(SharedConstants.ACTIONS.START_SERVICE)) {
             Log.i(TAG, "Received Start Service Intent ");
 
             Intent notificationIntent = new Intent(this, MainActivity.class); //open main activity when user clicks on notification
@@ -131,43 +140,54 @@ public class DataWriterService extends Service {
             notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
-            Intent recordIntent = new Intent(this, DataWriterService.class);
-            recordIntent.setAction(Constants.ACTION.RECORD_LABEL);
-            PendingIntent recordPendingIntent = PendingIntent.getService(this, 0, recordIntent, 0);
-
             Intent stopIntent = new Intent(this, DataWriterService.class);
-            stopIntent.setAction(Constants.ACTION.STOP_FOREGROUND);
+            stopIntent.setAction(SharedConstants.ACTIONS.STOP_SERVICE);
             PendingIntent stopPendingIntent = PendingIntent.getService(this, 0, stopIntent, 0);
 
-            Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_pill);
+            Bitmap icon = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_save);
 
             Notification notification = new NotificationCompat.Builder(this)
-                    .setContentTitle("My Gestures")
-                    .setTicker("My Gestures")
-                    .setContentText("Collecting Accelerometer/Gyroscope Data")
-                    .setSmallIcon(R.drawable.ic_pill)
+                    .setContentTitle(getString(R.string.app_name))
+                    .setTicker(getString(R.string.app_name))
+                    .setContentText(getString(R.string.notification_text))
+                    .setSmallIcon(android.R.drawable.ic_menu_save)
                     .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
                     .setContentIntent(pendingIntent)
                     .setPriority(Notification.PRIORITY_MAX) //otherwise buttons will not show up!
                     .setOngoing(true)
-                    .addAction(android.R.drawable.ic_btn_speak_now, "Record Label", recordPendingIntent)
-                    .addAction(android.R.drawable.ic_delete, "Stop Service", stopPendingIntent).build(); //got rid of notification actions, could use them later?
+                    .addAction(android.R.drawable.ic_delete, getString(R.string.stop_service), stopPendingIntent).build();
 
-            startForeground(Constants.NOTIFICATION_ID.FOREGROUND_SERVICE, notification);
-        } else if (intent.getAction().equals(Constants.ACTION.RECORD_LABEL)) {
-            Log.i(TAG, "Clicked Record Label");
-            Toast.makeText(getApplicationContext(), "Please record labels using the wearable device!", Toast.LENGTH_LONG).show();
-        } else if (intent.getAction().equals(Constants.ACTION.STOP_FOREGROUND)) {
+            startForeground(SharedConstants.NOTIFICATION_ID.DATA_WRITER_SERVICE, notification);
+        } else if (intent.getAction().equals(SharedConstants.ACTIONS.STOP_SERVICE)) {
             Log.i(TAG, "Received Stop Service Intent");
 
-            //make sure wearable sensor service stops when the service ends
-            RemoteSensorManager.getInstance(this).stopSensorService();
+            unregisterReceiver(receiver);
+            closeAllWriters();
 
             stopForeground(true);
             stopSelf();
         }
 
         return START_STICKY;
+    }
+
+    private void closeAllWriters(){
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(accelerometerWearableWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(gyroscopeWearableWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(accelerometerPhoneWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(gyroscopePhoneWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(accelerometerMetawearWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(gyroscopeMetawearWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(rssiMetawearToPhoneWriter);
+        if (accelerometerWearableWriter != null)
+            FileUtil.closeWriter(rssiMetawearToWearableWriter);
     }
 
     @Override

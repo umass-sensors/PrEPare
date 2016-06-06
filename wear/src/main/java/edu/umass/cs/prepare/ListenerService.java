dccea304e341985550a3ca0030e4517b7729e4f0
giffy1 <giffy1991@gmail.com>
2016-06-06 14:57:50 -0400

    Respond to commands sent from the handheld application to start/stop beacon service and to cancel the Metawear board connection; also removed code that parsed the Metawear address or delivered the preference map received on the wearable. All preferences are now handled by the wearprefs library.

--- wear/src/main/java/edu/umass/cs/prepare/ListenerService.java
+++ wear/src/main/java/edu/umass/cs/prepare/ListenerService.java
@@ -1,7 +1,6 @@
 package edu.umass.cs.prepare;
 
 import android.content.Intent;
-import android.util.Log;
 
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
@@ -18,12 +17,12 @@ import edu.umass.cs.shared.SharedConstants;
  * @affiliation University of Massachusetts Amherst
  */
 public class ListenerService extends WearableListenerService {
+    @SuppressWarnings("unused")
     /** used for debugging purposes */
     private static final String TAG = ListenerService.class.getName();
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
-        Log.d(TAG, "Received Message");
         if (messageEvent.getPath().equals(SharedConstants.COMMANDS.START_SENSOR_SERVICE)) {
             Intent startServiceIntent = new Intent(this, SensorService.class);
             startServiceIntent.setAction(SharedConstants.ACTIONS.START_SERVICE);
@@ -32,29 +31,26 @@ public class ListenerService extends WearableListenerService {
             Intent stopServiceIntent = new Intent(this, SensorService.class);
             stopServiceIntent.setAction(SharedConstants.ACTIONS.STOP_SERVICE);
             startService(stopServiceIntent);
-
-            //note: we call startService() instead of stopService() and pass in an intent with the stop service action,
-            //so that the service can unregister the sensors and do anything else it needs to do and then call stopSelf()
         }else if (messageEvent.getPath().equals(SharedConstants.COMMANDS.START_METAWEAR_SERVICE)) {
-            Log.d(TAG, "Received Message: Start metawear service");
-//            String mwMacAddress = "";
-//            try {
-//                mwMacAddress = new String(messageEvent.getData(), "UTF-8");
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
-//            Log.d(TAG, "Mac ID is " + mwMacAddress);
             Intent startServiceIntent = new Intent(this, edu.umass.cs.prepare.metawear.SensorService.class);
             startServiceIntent.setAction(SharedConstants.ACTIONS.START_SERVICE);
-            startServiceIntent.putExtra(SharedConstants.KEY.PREFERENCES, messageEvent.getData());
             startService(startServiceIntent);
         }else if (messageEvent.getPath().equals(SharedConstants.COMMANDS.STOP_METAWEAR_SERVICE)) {
             Intent stopServiceIntent = new Intent(this, edu.umass.cs.prepare.metawear.SensorService.class);
             stopServiceIntent.setAction(SharedConstants.ACTIONS.STOP_SERVICE);
             startService(stopServiceIntent);
-
-            //note: we call startService() instead of stopService() and pass in an intent with the stop service action,
-            //so that the service can unregister the sensors and do anything else it needs to do and then call stopSelf()
+        }else if (messageEvent.getPath().equals(SharedConstants.COMMANDS.START_BEACON_SERVICE)) {
+            Intent startPermissionsActivity = new Intent(this, PermissionsActivity.class);
+            startPermissionsActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startPermissionsActivity);
+        }else if (messageEvent.getPath().equals(SharedConstants.COMMANDS.STOP_BEACON_SERVICE)) {
+            Intent stopServiceIntent = new Intent(this, edu.umass.cs.prepare.metawear.BeaconService.class);
+            stopServiceIntent.setAction(SharedConstants.ACTIONS.STOP_SERVICE);
+            startService(stopServiceIntent);
+        }else if (messageEvent.getPath().equals(SharedConstants.COMMANDS.CANCEL_METAWEAR_CONNECTION)) {
+            Intent cancelServiceIntent = new Intent(this, edu.umass.cs.prepare.metawear.SensorService.class);
+            cancelServiceIntent.setAction(SharedConstants.ACTIONS.CANCEL_CONNECTING);
+            startService(cancelServiceIntent);
         }
     }
 }

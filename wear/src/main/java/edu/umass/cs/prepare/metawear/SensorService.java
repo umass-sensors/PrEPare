846b8a3c0aa9544da2c49bb971ee3100fe39711d
giffy1 <giffy1991@gmail.com>
2016-06-06 15:19:26 -0400

    When the Metawear BLE service is connected, send a message back to the handheld application.

--- wear/src/main/java/edu/umass/cs/prepare/metawear/SensorService.java
+++ wear/src/main/java/edu/umass/cs/prepare/metawear/SensorService.java
@@ -3,12 +3,15 @@ package edu.umass.cs.prepare.metawear;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import edu.umass.cs.prepare.PermissionsActivity;
 import edu.umass.cs.prepare.R;
 import edu.umass.cs.shared.BatteryUtil;
 import edu.umass.cs.shared.SharedConstants;
@@ -50,6 +53,7 @@ public class SensorService extends edu.umass.cs.shared.metawear.SensorService {
     @Override
     protected void onMetawearConnected(){
         startForeground();
+        client.sendMessage(SharedConstants.MESSAGES.METAWEAR_CONNECTED);
         setOnBufferFullCallback(accelerometerBuffer, new SensorBuffer.OnBufferFullCallback() {
             @Override
             public void onBufferFull(String[] timestamps, float[] values) {
@@ -74,7 +78,7 @@ public class SensorService extends edu.umass.cs.shared.metawear.SensorService {
 
     @Override
     protected void onRSSIReadingReceived(String timestamp, int rssi){
-        Log.d(TAG, String.valueOf(rssi));
+        //Log.d(TAG, String.valueOf(rssi));
     }
 
     private void startForeground(){
@@ -116,5 +120,11 @@ public class SensorService extends edu.umass.cs.shared.metawear.SensorService {
     protected void onBatteryLevelReceived(int percentage){
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(SharedConstants.NOTIFICATION_ID.METAWEAR_SENSOR_SERVICE, getUpdatedNotification(percentage));
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        client.sendMessage(SharedConstants.MESSAGES.METAWEAR_CONNECTING);
+        super.onServiceConnected(name, service);
     }
 }

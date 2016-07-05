4e45822146f24ffdd889b9864b30eddf64954621
giffy1 <giffy1991@gmail.com>
2016-07-05 11:08:54 -0400

    Check if the service is enabled before restarting it upon boot.

--- wear/src/main/java/edu/umass/cs/prepare/main/BootCompleteReceiver.java
+++ wear/src/main/java/edu/umass/cs/prepare/main/BootCompleteReceiver.java
@@ -3,8 +3,12 @@ package edu.umass.cs.prepare.main;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
+import edu.umass.cs.prepare.R;
 import edu.umass.cs.prepare.metawear.SensorService;
+import edu.umass.cs.shared.SharedConstants;
 
 /**
  * This class handles watch reboot events. If the wearable is rebooted then
@@ -14,10 +18,14 @@ public class BootCompleteReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        Intent service = new Intent(context, SensorService.class);
-        context.startService(service);
-
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean enabled = preferences.getBoolean(context.getString(R.string.pref_connect_key),
+                context.getResources().getBoolean(R.bool.pref_connect_default));
+        if (enabled) {
+            Intent service = new Intent(context, SensorService.class);
+            service.setAction(SharedConstants.ACTIONS.START_SERVICE);
+            context.startService(service);
+        }
     }
 
 }

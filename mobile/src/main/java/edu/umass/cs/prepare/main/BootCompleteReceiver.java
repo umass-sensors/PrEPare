d32124cbcdd05a4c7cdc6f9d0ab2232d3d85101c
giffy1 <giffy1991@gmail.com>
2016-07-05 11:08:25 -0400

    Handle boot event on the mobile device: If the service was previously running, then it should be restarted upon boot.

---
+++ mobile/src/main/java/edu/umass/cs/prepare/main/BootCompleteReceiver.java
@@ -0,0 +1,28 @@
+package edu.umass.cs.prepare.main;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import edu.umass.cs.prepare.R;
+import edu.umass.cs.prepare.communication.local.ServiceManager;
+import edu.umass.cs.prepare.metawear.SensorService;
+
+/**
+ * This class handles watch reboot events. If the mobile device is rebooted then
+ * the {@link SensorService} should be restarted.
+ */
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean enabled = preferences.getBoolean(context.getString(R.string.pref_connect_key),
+                context.getResources().getBoolean(R.bool.pref_connect_default));
+        if (enabled)
+            ServiceManager.getInstance(context).startMetawearService();
+    }
+
+}

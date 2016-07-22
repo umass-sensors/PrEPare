8409b68d603e37232a30b6b451c3806d0b8ce331
giffy1 <giffy1991@gmail.com>
2016-07-21 23:29:59 -0400

    Added splash screen.

--- mobile/src/main/java/edu/umass/cs/prepare/view/activities/SplashScreenActivity.java
+++ mobile/src/main/java/edu/umass/cs/prepare/view/activities/SplashScreenActivity.java
@@ -1,7 +1,17 @@
 package edu.umass.cs.prepare.view.activities;
 
-/**
- * Created by snoran on 7/18/16.
- */
-public class SplashScreenActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }

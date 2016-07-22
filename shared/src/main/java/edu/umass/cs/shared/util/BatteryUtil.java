82799c21cec8a113f975de7e084b23cf0581ccdb
giffy1 <giffy1991@gmail.com>
2016-07-22 00:10:05 -0400

    Added method to compute the battery lifetime given its remaining percentage. Also use the white-style icons in place of the old icons.

--- shared/src/main/java/edu/umass/cs/shared/util/BatteryUtil.java
+++ shared/src/main/java/edu/umass/cs/shared/util/BatteryUtil.java
@@ -11,6 +11,20 @@ import edu.umass.cs.shared.R;
  */
 public class BatteryUtil {
 
+    public static final double TOTAL_LIFETIME = 70; // hours
+
+    /**
+     * Computes the estimated lifetime of the Metawear battery, given the current battery level.
+     * The lifetime refers to time collecting data and not the physical lifetime, i.e. downtime
+     * is not included in the measurement.
+     *
+     * @param batteryLevel the current battery level in [0,100]
+     * @return an estimate of the remaining battery lifetime
+     */
+    public static double getBatteryLifetimeEstimate(int batteryLevel){
+        return BatteryUtil.TOTAL_LIFETIME * (batteryLevel / 100.0);
+    }
+
     /**
      * Returns the auto-generated integer ID associated with the icon that best captures
      * the specified battery level.
@@ -19,25 +33,25 @@ public class BatteryUtil {
      */
     public static int getBatteryLevelIconId(int percentage){
         if (percentage >= 95){
-            return R.drawable.ic_battery_full;
+            return R.drawable.ic_battery_full_white;
         }else if (percentage >= 85){
-            return R.drawable.ic_battery_90;
+            return R.drawable.ic_battery_90_white;
         }else if (percentage >= 75){
-            return R.drawable.ic_battery_80;
+            return R.drawable.ic_battery_80_white;
         }else if (percentage >= 65){
-            return R.drawable.ic_battery_70;
+            return R.drawable.ic_battery_70_white;
         }else if (percentage >= 55){
-            return R.drawable.ic_battery_60;
+            return R.drawable.ic_battery_60_white;
         }else if (percentage >= 45){
-            return R.drawable.ic_battery_50;
+            return R.drawable.ic_battery_50_white;
         }else if (percentage >= 30){
-            return R.drawable.ic_battery_40;
+            return R.drawable.ic_battery_40_white;
         }else if (percentage >= 15){
-            return R.drawable.ic_battery_30;
+            return R.drawable.ic_battery_30_white;
         }else if (percentage >= 5){
-            return R.drawable.ic_battery_low;
+            return R.drawable.ic_battery_low_white;
         }else if (percentage >= 1){
-            return R.drawable.ic_battery_critical;
+            return R.drawable.ic_battery_critical_white;
         }else{
             return R.drawable.ic_battery_empty;
         }

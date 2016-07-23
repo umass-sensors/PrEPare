20ab9dc8d63065bece72a4552b7528ce696406d9
giffy1 <giffy1991@gmail.com>
2016-07-22 23:41:25 -0400

    Added command to query the sensor service connection status.

--- wear/src/main/java/edu/umass/cs/prepare/communication/ListenerService.java
+++ wear/src/main/java/edu/umass/cs/prepare/communication/ListenerService.java
@@ -43,6 +43,10 @@ public class ListenerService extends WearableListenerService {
             Intent stopServiceIntent = new Intent(this, edu.umass.cs.prepare.metawear.SensorService.class);
             stopServiceIntent.setAction(SharedConstants.ACTIONS.STOP_SERVICE);
             startService(stopServiceIntent);
+        }else if (messageEvent.getPath().equals(SharedConstants.COMMANDS.QUERY_WEARABLE_STATE)) {
+            Intent stopServiceIntent = new Intent(this, SensorService.class);
+            stopServiceIntent.setAction(SharedConstants.ACTIONS.QUERY_CONNECTION_STATE);
+            startService(stopServiceIntent);
         }
     }
 }

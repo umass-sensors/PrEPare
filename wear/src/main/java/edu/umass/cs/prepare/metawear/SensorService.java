2de95207d74090732a9e61ea9656302faa3ccc42
giffy1 <giffy1991@gmail.com>
2016-07-26 12:30:34 -0400

    Update the service notifications depending on the current Metawear connection status.

--- wear/src/main/java/edu/umass/cs/prepare/metawear/SensorService.java
+++ wear/src/main/java/edu/umass/cs/prepare/metawear/SensorService.java
@@ -3,22 +3,31 @@ package edu.umass.cs.prepare.metawear;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 
-import edu.umass.cs.prepare.R;
+import java.util.Locale;
+
 import edu.umass.cs.prepare.communication.Broadcaster;
+import edu.umass.cs.prepare.communication.DataClient;
 import edu.umass.cs.shared.util.BatteryUtil;
 import edu.umass.cs.shared.constants.SharedConstants;
 import edu.umass.cs.shared.util.SensorBuffer;
-import edu.umass.cs.prepare.communication.DataClient;
+import edu.umass.cs.prepare.R;
 
 /**
- * The Sensor Service is responsible for streaming sensor data from the Metawear tag to the
- * mobile device via the wearable device. Available sensors include the accelerometer,
- * gyroscope and the Metawear-to-wearable received signal strength indicator (RSSI).
+ * The Sensor Service is responsible for streaming sensor data from the
+ * <a href="https://mbientlab.com/metawearc/">MetaWear C</a> tag to the phone. The data streams
+ * include accelerometer, gyroscope and the received signal strength indicator (RSSI) between
+ * the tag and the phone.
+ * <br> <hr> <br>
+ * To begin collecting sensor data, bind to the {@link SensorService} and start an intent
+ * with action {@link edu.umass.cs.shared.constants.SharedConstants.ACTIONS#START_SERVICE},
+ * passing in a {@link String} extra with key {@link SharedConstants.KEY#UUID}
+ * to identify the Bluetooth device by its address. To stop the service, use action
+ * {@link edu.umass.cs.shared.constants.SharedConstants.ACTIONS#STOP_SERVICE}.
  *
  * @author Sean Noran
  * @affiliation University of Massachusetts Amherst
@@ -28,6 +37,18 @@ public class SensorService extends edu.umass.cs.shared.metawear.SensorService {
     @SuppressWarnings("unused")
     /** used for debugging purposes */
     private static final String TAG = SensorService.class.getName();
+
+    private enum CONTENT_TEXT_IDENTIFIER {
+        COLLECTING_DATA,
+        LISTENING_FOR_MOVEMENT,
+        BLUETOOTH_DISABLED
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        super.onServiceConnected(name, service);
+        showForegroundNotification(CONTENT_TEXT_IDENTIFIER.LISTENING_FOR_MOVEMENT);
+    }
 
     /** used to communicate with the handheld application */
     private DataClient client;
@@ -58,59 +79,77 @@ public class SensorService extends edu.umass.cs.shared.metawear.SensorService {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent != null)
-            if (intent.getAction().equals(SharedConstants.ACTIONS.QUERY_BATTERY_LEVEL)){
-                queryBatteryLevel();
-            }
-        return super.onStartCommand(intent, flags, startId);
+    protected void onMetawearConnected(){
+        super.onMetawearConnected();
+        showForegroundNotification(CONTENT_TEXT_IDENTIFIER.COLLECTING_DATA);
+        queryBatteryLevel();
     }
 
     @Override
-    protected void onMetawearConnected(){
-        super.onMetawearConnected();
-        //queryBatteryLevel();
-        startForeground();
+    protected void onMetawearDisconnected() {
+        if (disconnectSource == DISCONNECT_SOURCE.BLUETOOTH_DISABLED){
+            showForegroundNotification(CONTENT_TEXT_IDENTIFIER.BLUETOOTH_DISABLED);
+        }else {
+            showForegroundNotification(CONTENT_TEXT_IDENTIFIER.LISTENING_FOR_MOVEMENT);
+        }
+        super.onMetawearDisconnected();
     }
 
-    private void startForeground(){
-        Notification notification = getUpdatedNotification(-1);
+    private void showForegroundNotification(CONTENT_TEXT_IDENTIFIER state){
+        Notification notification = getUpdatedNotification(-1, state);
         startForeground(SharedConstants.NOTIFICATION_ID.METAWEAR_SENSOR_SERVICE, notification);
     }
 
-    private Notification getUpdatedNotification(int batteryLevel){
-        Intent stopIntent = new Intent(this, SensorService.class);
-        stopIntent.setAction(SharedConstants.ACTIONS.STOP_SERVICE);
-        PendingIntent stopPendingIntent = PendingIntent.getService(this, 0, stopIntent, 0);
-
+    /**
+     * Returns the notification displayed during background data collection.
+     * @param batteryLevel The current battery level
+     * @return the notification handle
+     */
+    private Notification getUpdatedNotification(int batteryLevel, CONTENT_TEXT_IDENTIFIER state){
         Intent queryBatteryLevelIntent = new Intent(this, SensorService.class);
         queryBatteryLevelIntent.setAction(SharedConstants.ACTIONS.QUERY_BATTERY_LEVEL);
         PendingIntent queryBatteryLevelPendingIntent = PendingIntent.getService(this, 0, queryBatteryLevelIntent, 0);
-
-        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_pill);
 
         // notify the user that the foreground service has started
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setContentTitle(getString(R.string.app_name))
                 .setTicker(getString(R.string.app_name))
-                .setContentText(getString(R.string.sensor_service_notification))
-                .setSmallIcon(R.drawable.ic_pill)
-                .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
-                .setOngoing(true)
-                .setVibrate(new long[]{0, 50, 150, 200})
-                .setPriority(Notification.PRIORITY_MAX)
-                .addAction(android.R.drawable.ic_delete, getString(R.string.stop_service), stopPendingIntent);
+                .setSmallIcon(R.drawable.ic_notification)
+                .setOngoing(true);
 
-        if (batteryLevel > 0)
-                notificationBuilder = notificationBuilder.addAction(BatteryUtil.getBatteryLevelIconId(batteryLevel),
-                        SharedConstants.ACTIONS.QUERY_BATTERY_LEVEL, queryBatteryLevelPendingIntent);
+        String contentText;
+        if (state == CONTENT_TEXT_IDENTIFIER.COLLECTING_DATA){
+            contentText = getString(R.string.connection_notification);
+            notificationBuilder = notificationBuilder.setVibrate(new long[]{0, 50, 150, 200});
+        }else if (state == CONTENT_TEXT_IDENTIFIER.LISTENING_FOR_MOVEMENT) {
+            contentText = getString(R.string.sensor_service_notification);
+        }else if (state == CONTENT_TEXT_IDENTIFIER.BLUETOOTH_DISABLED){
+            contentText = "Please enable Bluetooth";
+        }else{
+            contentText = "";
+        }
+        if (batteryLevel >= 0)
+            contentText += String.format(Locale.getDefault(), "(battery: %d%%)", batteryLevel);
+        notificationBuilder = notificationBuilder.setContentText(contentText);
+
+        if (batteryLevel >= 0)
+            notificationBuilder = notificationBuilder.addAction(BatteryUtil.getBatteryLevelIconId(batteryLevel),
+                    "Battery Level", queryBatteryLevelPendingIntent);
 
         return notificationBuilder.build();
     }
 
-    @Override
-    protected void onBatteryLevelReceived(int percentage){
+    private void showBatteryLevelNotification(int percentage){
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(SharedConstants.NOTIFICATION_ID.METAWEAR_SENSOR_SERVICE, getUpdatedNotification(percentage));
+        notificationManager.notify(SharedConstants.NOTIFICATION_ID.METAWEAR_SENSOR_SERVICE,
+                getUpdatedNotification(percentage, CONTENT_TEXT_IDENTIFIER.COLLECTING_DATA));
     }
+
+    @Override
+    protected void onBatteryLevelReceived(int percentage) {
+        showBatteryLevelNotification(percentage);
+        client.sendSensorData(SharedConstants.SENSOR_TYPE.RSSI, new long[]{System.currentTimeMillis()}, new float[]{percentage});
+    }
+
 }
+
